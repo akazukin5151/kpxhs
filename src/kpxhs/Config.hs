0be1fe7d413d2745b6b82d80110261be58fcc9d5
twenty5151 <tsuiyikching@protonmail.com>
2021-08-27 21:04:17 +0100

    Use Data.ByteString.readFile instead of Data.Text.IO

--- src/kpxhs/Config.hs
+++ src/kpxhs/Config.hs
@@ -9,11 +9,12 @@ import           Brick.Util                    (on)
 import           Control.Exception             (IOException)
 import           Control.Exception.Base        (catch)
 import           Data.Bifunctor                (second)
+import qualified Data.ByteString               as B
 import           Data.Functor                  ((<&>))
 import           Data.Maybe                    (fromMaybe)
 import           Data.Text                     (Text, unpack)
 import qualified Data.Text                     as TT
-import qualified Data.Text.IO                  as TTI
+import           Data.Text.Encoding            (decodeUtf8')
 import           Graphics.Vty                  (Attr, Color (ISOColor))
 import           Graphics.Vty.Attributes       (withStyle)
 import           Graphics.Vty.Attributes.Color (rgbColor)
@@ -29,19 +30,22 @@ import Types
     )
 
 
-fallback :: IOException -> IO Text
+fallback :: IOException -> IO B.ByteString
 fallback _ = pure ""
 
 parseConfig :: String -> IO (Maybe Int, Text, Text, F.FocusRing Field, Theme)
 parseConfig cfgdir = do
-  file <- catch (TTI.readFile $ cfgdir <> "config") fallback
+  file <- catch (B.readFile $ cfgdir <> "config") fallback
   attrMap <- parseTheme $ cfgdir <> "theme.hs"
-  let parsed   = parseSettings (TT.lines file) initial
-  let timeout' = fromMaybe (Just 10) (timeout parsed)
-  let db_path  = fromMaybe "" (dbPath parsed)
-  let kf_path  = fromMaybe "" (keyfilePath parsed)
-  let ring     = if db_path == "" then pathfirst else passwordfirst
-  pure (timeout', db_path, kf_path, ring, attrMap)
+  case decodeUtf8' file of
+    Left _ -> pure (Just 10, "", "", pathfirst, attrMap)
+    Right utf8_file -> do
+      let parsed   = parseSettings (TT.lines utf8_file) initial
+      let timeout' = fromMaybe (Just 10) (timeout parsed)
+      let db_path  = fromMaybe "" (dbPath parsed)
+      let kf_path  = fromMaybe "" (keyfilePath parsed)
+      let ring     = if db_path == "" then pathfirst else passwordfirst
+      pure (timeout', db_path, kf_path, ring, attrMap)
   where
     initial = Setting { timeout     = Nothing
                       , dbPath      = Nothing
@@ -65,8 +69,11 @@ eval (WithStyle a s) = withStyle (eval a) s
 
 parseTheme :: FilePath -> IO [(AttrName, Attr)]
 parseTheme theme_path = do
-  file <- catch (TTI.readFile theme_path) fallback
-  let theme_aux = fromMaybe defaultTheme (readMaybe $ unpack file)
+  file <- catch (B.readFile theme_path) fallback
+  let theme_aux = either
+        (const defaultTheme)
+        (fromMaybe defaultTheme . readMaybe . unpack)
+        (decodeUtf8' file)
   -- second eval === (\(a, b) -> (a, eval b))
   pure $ theme_aux <&> second eval
 
